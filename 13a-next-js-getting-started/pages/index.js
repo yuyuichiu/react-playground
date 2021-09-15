0526d1ef94b6f50a469c22c53fc82e5a0613d405
MaxwellYu <yuichiuyu1915@gmail.com>
2021-09-15 22:24:23 +0800

    .

--- 13a-next-js-getting-started/pages/index.js
+++
@@ -1,5 +0,0 @@
-// The home page of our domain
-
-export default function HomePage(props) {
-  return (<h1>Welcome to the home page!</h1>)
-}
--- 13b-nextjs-meetup-app/pages/[meetupId]/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Fragment } from "react"
-import Header from "../../components/header/Header"
-
-export default function MeetupDetail(props) {
-  return (<Fragment>
-    <Header />
-  </Fragment>)
-}
